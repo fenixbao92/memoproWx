311a7194b14ae6594457f92f77227a5d13ce6753
fenixbao92 <fenixbao92@gmail.com>
2019-05-27 14:09:22 +0800

    update
    update

--- page/todo/index.js
+++ page/todo/index.js
@@ -13,7 +13,61 @@ Page({
   },
 
   data: {
+    tabTxt: ['日期', '状态', '标签'],
+    tab: [true, true, true],
+    statusList: ["未开始", "进行中", "暂停中", "已完成",'无'],
+    selectStatus: '',
+    tagList: ["工作", "学习", "实践", "阅读", "生活", "其它",'无'],
+    selectTag: '',
+    selectDate: '',
     list: []
+  },
+
+  // 选项卡
+  filterTab: function (e) {
+    var data = [true, true, true];
+    var index = e.currentTarget.dataset.index;
+    data[index] = !this.data.tab[index];
+    this.setData({
+      tab: data
+    })
+  },
+  //筛选项点击操作
+  filter: function (e) {
+    var self = this, txt = e.currentTarget.dataset.txt, tabTxt = this.data.tabTxt;
+    var index = parseInt(e.currentTarget.dataset.index);
+    if(txt!='无'){
+      tabTxt[index] = txt;
+    }else{
+      if (!this.data.tab[0]){
+        tabTxt[0]='日期';
+      }
+      if (!this.data.tab[1]) {
+        tabTxt[1] = '状态';
+      }
+      if (!this.data.tab[2]) {
+        tabTxt[2] = '标签';
+      }
+    }
+    if (!this.data.tab[0]){
+      this.setData({ selectDate:(txt=='无'?"":txt)});
+    }
+    if (!this.data.tab[1]) {
+      this.setData({ selectStatus: (txt == '无' ? "" : txt) });
+    }
+    if (!this.data.tab[2]) {
+      this.setData({ selectTag: (txt == '无' ? "" : txt) });
+    }
+    this.setData({ tabTxt: tabTxt });
+    this.setData({ tab: [true,true,true] })
+    //数据筛选
+    self.getDataList();
+  },
+
+  getDataList(){
+    console.log(this.data.selectDate);
+    console.log(this.data.selectStatus);
+    console.log(this.data.selectTag);
   },
 
   onLoad() {
@@ -34,12 +88,8 @@ Page({
         list: res.todos
       })
     });
-    var s = "2019-05-20T02:25:31.000+0000";
-    console.log(s);
-    var d = new Date(s);
-    console.log(d);
-  },
-  onReady: function(e) {
-    this.list = this.selectComponent("#todolist");
   }
+  // onReady: function(e) {
+  //   this.list = this.selectComponent("#todolist");
+  // }
 })
